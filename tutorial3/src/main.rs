39cde5492a3de15c4fdf777b7215cdb3aeeb8b9e
rai64521 <raisumant99@gmail.com>
2025-09-08 17:46:35 +0530

    let const in rust

---
+++ tutorial3/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut number = 5;
+    println!("number is: {}", number);
+    number = 55;
+    println!("number is: {}", number);
+
+    const SECOND_IN_MINUTE: u32 = 60;
+    println!("{}", SECOND_IN_MINUTE)
+  
+}
