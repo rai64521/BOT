49a5e1b8be5478dbf2e4477de5c0c01ef797a8c3
rai64521 <raisumant99@gmail.com>
2025-09-09 21:07:04 +0530

    aipromo

---
+++ foret/target/debug/deps/foret-00e2b1d4784d2f3b.d
@@ -0,0 +1,5 @@
+C:\Users\HP\Desktop\RUST\foret\target\debug\deps\foret-00e2b1d4784d2f3b.d: src\main.rs
+
+C:\Users\HP\Desktop\RUST\foret\target\debug\deps\libforet-00e2b1d4784d2f3b.rmeta: src\main.rs
+
+src\main.rs:
---
+++ foret/target/debug/deps/foret-13b3a6675b8e978e.d
@@ -0,0 +1,5 @@
+C:\Users\HP\Desktop\RUST\foret\target\debug\deps\foret-13b3a6675b8e978e.d: src\main.rs
+
+C:\Users\HP\Desktop\RUST\foret\target\debug\deps\libforet-13b3a6675b8e978e.rmeta: src\main.rs
+
+src\main.rs:
