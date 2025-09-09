5e1735fbb68e1b05e064fbf832e7a0cbdec957d1
rai64521 <raisumant99@gmail.com>
2025-09-09 20:50:04 +0530

    cargo add

---
+++ bot/src/main.rs
@@ -0,0 +1,19 @@
+use teloxide::prelude::*;
+use dotenv::dotenv;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok(); 
+
+    pretty_env_logger::init();
+    log::info!("Telegram bot...");
+
+    let bot = Bot::from_env().auto_send();
+
+    teloxide::repl(bot, |message| async move {
+        message.answer("Hey, there i am sumant rai!").send().await?;
+        Ok(())
+    })
+    .await;
+}
